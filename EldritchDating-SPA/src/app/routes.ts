a6ba6635077f20d74358347f188fccd6c093ef43
Dirac Lotus <esenft1993@gmail.com>
2020-06-06 11:23:42 +0100

    AuthGuard routes. End of section 7

--- EldritchDating-SPA/src/app/routes.ts
+++ EldritchDating-SPA/src/app/routes.ts
@@ -3,12 +3,20 @@ import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
     // Ordering is important - first wins matching
-    { path: 'home', component: HomeComponent },
-    { path: 'lists', component: ListsComponent },
-    { path: 'members', component: MemberListComponent },
-    { path: 'messages', component: MessagesComponent },
-    { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    { path: '', component: HomeComponent },
+    {
+        path: '', 
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'lists', component: ListsComponent },
+            { path: 'members', component: MemberListComponent },
+            { path: 'messages', component: MessagesComponent },
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
