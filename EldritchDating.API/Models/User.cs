8b455d67ba1a002addd484bf6bfdd03f0d5e01c3
Dirac Lotus <esenft1993@gmail.com>
2020-06-07 07:48:12 +0100

    Seed data into the users table

--- EldritchDating.API/Models/User.cs
+++ EldritchDating.API/Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace EldritchDating.API.Models
 {
     public class User
@@ -6,5 +9,14 @@ namespace EldritchDating.API.Models
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Devotion { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string Location { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
--- EldritchDating.API/Program.cs
+++ EldritchDating.API/Program.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using EldritchDating.API.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +12,24 @@ namespace EldritchDating.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var hosts = CreateHostBuilder(args).Build();
+            using (var scope = hosts.Services.CreateScope()) 
+            {
+                var services = scope.ServiceProvider;
+                try 
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                    Seed.SeedUsers(context);
+                } 
+                catch (Exception ex) 
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred during migration");
+                }
+            }
+
+            hosts.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
