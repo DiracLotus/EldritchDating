99ea1c64ee5daa893da8d9814fade8306b9b34e7
Dirac Lotus <esenft1993@gmail.com>
2020-06-09 17:19:25 +0100

    Add image gallery

--- EldritchDating-SPA/src/app/app.module.ts
+++ EldritchDating-SPA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery'
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -46,6 +47,7 @@ export function tokenGetter() {
       BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
       RouterModule.forRoot(appRoutes),
+      NgxGalleryModule,
       TabsModule.forRoot(),
       JwtModule.forRoot({
          config: {
