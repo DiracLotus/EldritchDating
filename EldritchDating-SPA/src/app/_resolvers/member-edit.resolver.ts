6c930e616ed53949fefcd384e9f92f7a6e4eae2c
Dirac Lotus <esenft1993@gmail.com>
2020-06-09 21:16:24 +0100

    Add MemberEdit component

--- EldritchDating-SPA/src/app/_resolvers/member-edit.resolver.ts
+++ EldritchDating-SPA/src/app/_resolvers/member-edit.resolver.ts
@@ -5,19 +5,21 @@ import { UserService } from '../_services/user.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { of, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { AuthService } from '../_services/auth.service';
 
 @Injectable()
-export class MemberListResolver implements Resolve<User[]> {
+export class MemberEditResolver implements Resolve<User> {
     constructor(
         private userService: UserService, 
         private router: Router,
-        private alertify: AlertifyService) { }
+        private alertify: AlertifyService,
+        private authService: AuthService) { }
         
-    resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-        return this.userService.getUsers().pipe(
+    resolve(route: ActivatedRouteSnapshot): Observable<User> {
+        return this.userService.getUser(this.authService.decodedToken.nameid).pipe(
             catchError(error => {
-                this.alertify.error('Problem retrieving all data');
-                this.router.navigate(['/home']);
+                this.alertify.error('Problem retrieving your data');
+                this.router.navigate(['/members']);
                 return of(null);
             })
         )
--- EldritchDating-SPA/src/app/routes.ts
+++ EldritchDating-SPA/src/app/routes.ts
@@ -7,6 +7,8 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 export const appRoutes: Routes = [
     // Ordering is important - first wins matching
@@ -20,8 +22,10 @@ export const appRoutes: Routes = [
                 resolve: { users: MemberListResolver } },
             { path: 'members/:id', component: MemberDetailComponent,
                  resolve: {user: MemberDetailResolver} },
+            { path: 'member/edit', component: MemberEditComponent,
+                resolve: { user: MemberEditResolver } },
             { path: 'messages', component: MessagesComponent },
-            { path: 'lists', component: ListsComponent },
+            { path: 'lists', component: ListsComponent }
         ]
     },
     { path: '**', redirectTo: '', pathMatch: 'full' }
