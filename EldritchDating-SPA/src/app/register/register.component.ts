ca8d5e8061af2f5c124b6c142052b4251b0a8d58
Dirac Lotus <esenft1993@gmail.com>
2020-06-13 22:03:29 +0100

    Extend register component

--- EldritchDating-SPA/src/app/register/register.component.ts
+++ EldritchDating-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,18 +11,41 @@ import { AlertifyService } from '../_services/alertify.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService, 
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createRegisterForm();
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value 
+      ? null : { 'mismatch': true };
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(256)]],
+      confirmPassword: ['', Validators.required],
+      devotion: ['cultist'],
+      knownAs: ['', Validators.required],
+      location: ['', Validators.required],
+      dateOfBirth: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-       this.alertify.success('Registration successful');
-    }, error => {
-      this.alertify.error(error);
-    });
+    // this.authService.register(this.model).subscribe(() => {
+    //    this.alertify.success('Registration successful');
+    // }, error => {
+    //   this.alertify.error(error);
+    // });
+    console.log(this.registerForm.value);
   }
 
   cancel() {
